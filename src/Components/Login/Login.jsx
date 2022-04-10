c313ceaf5264c4af805d959fc2a314733a3b171a
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-10 20:20:39 +0600

    event handler added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -18,12 +18,12 @@ const Login = () => {
   };
 
   return (
-    <div className="container box-shadow sizing mx-auto border p-4  mt-4 mb-4">
+    <div className="container box-shadow sizing mx-auto border p-4  mt-5 mb-5">
       <h2 className="text-center mb-4">Login</h2>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control className="py-2" required type="email" />
+          <Form.Control className="py-2 shadow-none" required type="email" />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           <Form.Control.Feedback type="invalid">
             Please provide a valid a valid email
@@ -32,7 +32,7 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control className="py-2" required type="password" />
+          <Form.Control className="py-2 shadow-none" required type="password" />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           <Form.Control.Feedback type="invalid">
             Please provide your password
@@ -40,7 +40,7 @@ const Login = () => {
         </Form.Group>
         <button
           type="submit "
-          className="w-100 rounded-0 login-btn-bg-color py-2"
+          className="w-100 rounded-0 login-btn-bg-color py-2 shadow-none"
         >
           Login
         </button>
@@ -54,7 +54,7 @@ const Login = () => {
         <div className="content mt-4 mb-4">
           <p className="or">or</p>
         </div>
-        <button className="btn w-100 border py-3 mb-3">
+        <button className="btn w-100 border border-secondary py-3 mb-3 shadow-none">
           <img className="img-fluid me-2" src={googleLogo} alt="" /> Continue
           with Google
         </button>
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -5,23 +5,51 @@ import googleLogo from "../../images/google.svg";
 
 const SignUp = () => {
   const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleConfirmPasswordBluer = (event) => {
+    setEmail(event.target.value);
+  };
+  // const handleSubmit = () => {
+
+  // };
 
   const handleSubmit = (event) => {
+    event.preventDefault();
+
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
-
     setValidated(true);
+    if (password !== confirmPassword) {
+      setError("opps Password Not Match");
+      return;
+    }
+    // setError("");
   };
   return (
-    <div className="container box-shadow sizing mx-auto border p-4  mt-4 mb-4">
+    <div className="container box-shadow sizing mx-auto border p-4  mt-5 mb-5">
       <h2 className="text-center mb-4">Sign Up</h2>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control className="py-2" required type="email" />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            className="py-2 shadow-none"
+            required
+            type="email"
+          />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           <Form.Control.Feedback type="invalid">
             Please provide a valid a valid email
@@ -30,7 +58,12 @@ const SignUp = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control className="py-2" required type="password" />
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            className="py-2 shadow-none"
+            required
+            type="password"
+          />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           <Form.Control.Feedback type="invalid">
             Please provide your password
@@ -38,15 +71,21 @@ const SignUp = () => {
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control className="py-2" required type="password" />
+          <Form.Control
+            onBlur={handleConfirmPasswordBluer}
+            className="py-2 shadow-none"
+            required
+            type="password"
+          />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           <Form.Control.Feedback type="invalid">
             Please provide your Confirm Password
           </Form.Control.Feedback>
         </Form.Group>
+        <p className="text-danger">{error}</p>
         <button
           type="submit "
-          className="w-100 rounded-0 login-btn-bg-color py-2"
+          className="w-100 rounded-0 login-btn-bg-color py-2 shadow-none"
         >
           Sign Up
         </button>
@@ -60,7 +99,7 @@ const SignUp = () => {
         <div className="content mt-4 mb-4">
           <p className="or">or</p>
         </div>
-        <button className="btn w-100 border py-3 mb-3">
+        <button className="btn w-100 border border-secondary py-3 mb-3 shadow-none">
           <img className="img-fluid me-2" src={googleLogo} alt="" /> Continue
           with Google
         </button>
