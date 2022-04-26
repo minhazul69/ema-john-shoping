03eda29b54dd31519f8725a86123b3b22fdfae6d
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-26 20:46:10 +0600

    pagination update

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -6,16 +6,27 @@ const useCart = (products) => {
   useEffect(() => {
     const storedCart = getShoppingCart();
     const saveCart = [];
-    for (const id in storedCart) {
-      const addProduct = products.find((product) => product._id === id);
-      if (addProduct) {
-        const quantity = storedCart[id];
-        addProduct.quantity = quantity;
-        saveCart.push(addProduct);
-      }
-    }
-    setCart(saveCart);
-  }, [products]);
+    const keys = Object.keys(storedCart);
+    fetch("http://localhost:5000/productByKeys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(keys),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        for (const id in storedCart) {
+          const addProduct = products.find((product) => product._id === id);
+          if (addProduct) {
+            const quantity = storedCart[id];
+            addProduct.quantity = quantity;
+            saveCart.push(addProduct);
+          }
+        }
+        setCart(saveCart);
+      });
+  }, []);
   return [cart, setCart];
 };
 export default useCart;
